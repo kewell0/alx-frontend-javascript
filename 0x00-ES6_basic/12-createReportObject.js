0acce3f49663d1ea2074a4d25ddf8dfcf851d765
CALEB <owatahcaleb@gmail.com>
2022-12-20 05:32:53 +0100

    advance

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+    };
+  }
+  
