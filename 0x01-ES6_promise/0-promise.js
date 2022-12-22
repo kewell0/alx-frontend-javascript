11ebe1ff167fa0de93a4f797de757394c20b00de
CALEB <owatahcaleb@gmail.com>
2022-12-22 04:09:46 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
-    const flag = true;
-    return new Promise((resolve, reject) => {
-        if (flag) {
-            resolve();
-        } else {
-            reject();
-        }
-    })
+  const flag = true;
+  return new Promise((resolve, reject) => {
+    if (flag) {
+      resolve();
+    } else {
+      reject();
+  }
+  })
 }
